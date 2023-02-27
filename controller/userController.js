d78681ea9b0f1a5064a8a04ed0b56ef6269c7a9d
hoanganhle <55530429+Phoenixp655@users.noreply.github.com>
2023-02-27 02:19:58 +0000

    getAllUserExercise from userController now can select 'date  from to' and limit respone log exercise

--- controller/userController.js
+++ controller/userController.js
@@ -35,9 +35,9 @@ const createUserExercise = asyncHandler(async (req, res) => {
 
     //@ if date field empty use system date or check regex date string if valid then create date from it
     const date = !reqBody.date 
-    ? new Date(Date.now()).toDateString() 
+    ? new Date(Date.now()).toLocaleDateString().split('/').reverse().join('-') 
     : /[0-9]{4}(\/|\-)[0-9]{1,2}(\/|\-)[0-9]{1,2}/g.test(reqBody.date) 
-        ? new Date(reqBody.date).toDateString() 
+        ? reqBody.date
         : res.json({error: 'Invalid date format'});
 
     const creaExercise = await exerciseSchema.create({
@@ -57,7 +57,7 @@ const createUserExercise = asyncHandler(async (req, res) => {
                 username: getUser.username, 
                 description: data.description,
                 duration: data.duration,
-                date: data.date,
+                date: new Date(data.date).toDateString(),
                 _id: getUser._id
             })
     })
@@ -67,12 +67,19 @@ const createUserExercise = asyncHandler(async (req, res) => {
 //@ GET method
 const getAllUserExercises = asyncHandler(async (req, res) => {
     const reqParams =  req.params.id;
+    const {from: fromDate, to: toDate, limit: limitLog} = req.query;
+    console.log(req.query)
+  
     let getUser = await userSchema.findById(reqParams,'username');
-
+  
+await exerciseSchema.find({date : {$lte: new Date(toDate).toDateString(), $gte: new Date(fromDate).toDateString()}}).limit(limitLog||0).exec((err, data) => {
+        console.log(data)
+    })
     //@ query find exercise with exact id of user and res user with their exercise logs  
     await exerciseSchema.find({}, 'description duration date -_id').where('users').equals(reqParams).exec((err, data) => {
         if(err) console.log(data);
-        res.status(200).json({_id : reqParams, username : getUser.username, count : data.length, log: data})
+        let logs = data.map(e => e.date = new Date(e.date).toDateString());
+        res.status(200).json({_id : reqParams, username : getUser.username, count : data.length, log: logs})
     })
 })
 
